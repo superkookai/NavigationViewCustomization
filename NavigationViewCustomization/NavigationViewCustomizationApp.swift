50fb72f2438d17b488bcd5dbacd0859acf31f0f5
Weerawut <weerawut@live.com>
2024-12-10 17:02:35 +0700

    Initial Commit

---
+++ NavigationViewCustomization/NavigationViewCustomizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewCustomizationApp.swift
+//  NavigationViewCustomization
+//
+//  Created by Weerawut Chaiyasomboon on 10/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewCustomizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
